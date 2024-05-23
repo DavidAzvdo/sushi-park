93e9fbd66248850b01a0512a09b9eb349b994380
David Azevedo <davidazevedo.dev@gmail.com>
2024-05-23 10:41:59 -0300

    adicionado section de avaliaçoes e footer

--- src/script.js
+++ src/script.js
@@ -4,23 +4,5 @@ $(document).ready(function() {
     $('#mobile-btn').find('i').toggleClass('fa-x');
   });
 
-  // Defina currentIndex e totalSlides
-  let currentIndex = 0;
-  const totalSlides = $('.slide').length;
-
-  function showNextSlide() {
-    currentIndex++;
-    if (currentIndex >= totalSlides) {
-      currentIndex = 0;
-    }
-    updateSlidePosition();
-  }
-
-  function updateSlidePosition() {
-    const slidesContainer = document.querySelector('.slides');
-    slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-  }
-
-  // Defina o intervalo após o carregamento do documento
-  setInterval(showNextSlide, 5000);
+  
 });
